ba114015c33ba577c0134edfe8f38e88b7e0c8a5
Karan Rawal <kr14nov@outlook.com>
2022-03-14 16:00:41 +0530

    Added some comments for better understanding.

--- learnopengl/LearnOpenGL/LearnOpenGL/main.cpp
+++ learnopengl/LearnOpenGL/LearnOpenGL/main.cpp
@@ -11,25 +11,52 @@ void processInput(GLFWwindow *window) {
 }
 
 int main() {
+	// Initialize glfwInit
 	glfwInit();
+
+	// Hint glfw which opengl version we are goona use.
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	// Hint glfw we are using only core profile.
+	// So we have access to only modern functions.
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	// Create a glfw window and do the error handling.
 	GLFWwindow* window = glfwCreateWindow(800, 800, "learnopengl.com", NULL, NULL);
 	if (window == NULL) {
 		cout << "Failed to create window." << endl;
 		glfwTerminate();
 		return -1;
 	}
+	// Tell gltf to use the window as current context.
 	glfwMakeContextCurrent(window);
 
+	// tell glad to use glfwGetProcAddress to get the OS specific gl loaders.
+	// we can also use gladLoadGL() but not sure which is the correct way.
+	// we need to do this before doing anything related to open gl.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize glad." << endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
 
+	// open gl operations.
+	glViewport(0, 0, 800, 800);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	// tell glfw to swap front buffer with back buffer.
+	glfwSwapBuffers(window);
+
+	// the main window loop.
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
+		// process all pending events. Without this no events will be triggered.
 		glfwPollEvents();
 	}
 
+	// finally, destroy the window and terminate glfw.
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;

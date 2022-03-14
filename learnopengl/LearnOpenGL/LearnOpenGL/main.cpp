85e1aba025ca2016a1212ecffdb80cb89ab984c6
Karan Rawal <kr14nov@outlook.com>
2022-03-14 15:00:48 +0530

    Initializing glfw and showing a window

--- learnopengl/LearnOpenGL/LearnOpenGL/main.cpp
+++ learnopengl/LearnOpenGL/LearnOpenGL/main.cpp
@@ -3,5 +3,24 @@
 #include <GLFW/glfw3.h>
 
 int main() {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "learnopengl.com", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create window.";
+	}
+	glfwMakeContextCurrent(window);
+
+
+	while (true) {
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
